df1456f88e7a4758724f84c3abde0420b5704b9f
kenneth898 <kennethtan0601@gmail.com>
2024-09-10 17:48:25 +0800

    sitemap api robots

--- src/components/Left.vue
+++ src/components/Left.vue
@@ -4,11 +4,11 @@
 			<div class="row">
 				<article>
 					<!--One Stop Gaming Platform-->
-					<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img width="100%"
+					<a :href="link + 'sign-up'" rel="nofollow" target="_blank"><img width="100%"
 							src="/public/image/atas_one_stop_platform.jpg" alt="One Stop Gaming Platform"></a>
 					<br><br>
 					<!--188% Welcome Bonus-->
-					<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img width="100%"
+					<a :href="link + 'sign-up'" rel="nofollow" target="_blank"><img width="100%"
 							src="/public/image/188welcome_bonus.jpg" alt="Welcome Bonus"></a>
 
 					<!--Bank Logo-->
@@ -22,21 +22,21 @@
 				<div class="hide_mobile">
 					<article>
 						<!--baccarat largest bonus-->
-						<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img
-								width="100%" src="/public/image/challenge.jpg" alt="baccarat largest bonus"></a>
+						<a :href="link + 'sign-up'" rel="nofollow" target="_blank"><img width="100%"
+								src="/public/image/challenge.jpg" alt="baccarat largest bonus"></a>
 
 						<!--check in rm388-->
-						<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img
-								width="100%" src="/public/image/check_in.jpg" alt="check in rm388"></a>
+						<a :href="link + 'sign-up'" rel="nofollow" target="_blank"><img width="100%"
+								src="/public/image/check_in.jpg" alt="check in rm388"></a>
 
 						<!--Share & Downline & Copy Link button-->
 						<div class="btn_left">
-							<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img
-									width="50%" src="/public/image/share_btn.gif" alt="Share"></a>
-							<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img
-									width="50%" src="/public/image/downline_btn.gif" alt="Downline"></a>
-							<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img
-									width="50%" src="/public/image/copylink_btn.gif" alt="Copy Link button"></a>
+							<a :href="link + 'sign-up'" rel="nofollow" target="_blank"><img width="50%"
+									src="/public/image/share_btn.gif" alt="Share"></a>
+							<a :href="link + 'sign-up'" rel="nofollow" target="_blank"><img width="50%"
+									src="/public/image/downline_btn.gif" alt="Downline"></a>
+							<a :href="link + 'sign-up'" rel="nofollow" target="_blank"><img width="50%"
+									src="/public/image/copylink_btn.gif" alt="Copy Link button"></a>
 						</div>
 
 						<!--Lucky Draw Deposit-->
@@ -67,8 +67,33 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
 	name: 'Left',
+	data() {
+		return {
+			link: ""
+		}
+	},
+	methods: {
+		async calllink() {
+			try {
+				const response = await axios.get('https://seo.mobileapplab.online/api/atas?fields[0]=atascasinobet_com', {
+					headers: {
+						"Authorization": "Bearer " + "1c4db3188ab2e9a077928920d9cc8d3322d15f9751bc2054a5cb70008df79cf3e3a4dd005a75a1f2db40eb953292ee10ef699693e96e9d77a98439f438ee6a6e6805a8a955e992f082b9e6118a4345e1ed18438ff9789edf9ed1dd58af45ee6669a7519a1291746959ff45bc2054b7f408b5da5ea8cd04d588a2704b7e218021",
+					}
+				});
+				this.link = response.data.data.attributes.atascasinobet_com;
+
+				console.log(this.link);
+			} catch (error) {
+				console.error(error);
+			}
+		},
+	},
+	mounted() {
+		this.calllink();
+	}
 };
 </script>
 
--- src/components/Rightside.vue
+++ src/components/Rightside.vue
@@ -2,7 +2,7 @@
 	<div class="right_col">
 		<article>
 			<!--Daily unlimited top up bonus-->
-			<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img width="100%"
+			<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="100%"
 					src="/public/image/daily_top_up_bonus.jpg" alt="Daily unlimited top up bonus"></a>
 		</article>
 
@@ -14,8 +14,8 @@
 				<div class="col-6">
 					<article>
 						<!--Login btn-->
-						<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img
-								width="100%" src="/public/image/login_btn.gif" alt="Login btn"></a>
+						<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="100%"
+								src="/public/image/login_btn.gif" alt="Login btn"></a>
 					</article>
 
 				</div>
@@ -23,8 +23,8 @@
 				<div class="col-6">
 					<article>
 						<!--Register btn-->
-						<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img
-								width="100%" src="/public/image/register_btn.gif" alt="Register btn"></a>
+						<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="100%"
+								src="/public/image/register_btn.gif" alt="Register btn"></a>
 					</article>
 
 				</div>
@@ -52,16 +52,16 @@
 				<div class="col-6">
 					<article>
 						<!--Deposit btn-->
-						<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img
-								width="100%" src="/public/image/deposit_btn.gif" alt="Deposit btn"></a>
+						<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="100%"
+								src="/public/image/deposit_btn.gif" alt="Deposit btn"></a>
 						<br>
 						<!--Withdraw btn-->
-						<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img
-								width="100%" src="/public/image/withdraw_btn.gif" alt="Withdraw btn"></a>
+						<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="100%"
+								src="/public/image/withdraw_btn.gif" alt="Withdraw btn"></a>
 						<br>
 						<!--Refresh btn-->
-						<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img
-								width="100%" src="/public/image/refresh_btn.gif" alt="Refresh btn"></a>
+						<a :href="link + '/sign-up'" rel=" nofollow" target="_blank"><img width="100%"
+								src="/public/image/refresh_btn.gif" alt="Refresh btn"></a>
 					</article>
 				</div>
 			</div>
@@ -70,16 +70,16 @@
 		<div class="">
 			<article>
 				<!--Game Tips X Bet Ratio-->
-				<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img width="100%"
+				<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="100%"
 						src="/public/image/game_tips_x_bet_ratio.gif" alt="Game Tips X Bet Ratio"></a>
 
 
 				<!--welcome bonus 188%-->
-				<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img width="100%"
+				<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="100%"
 						src="/public/image/Atas-Post-Banner-(Eng).jpg" alt="welcome bonus"></a>
 
 				<!--percuma rm88-->
-				<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img width="100%"
+				<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="100%"
 						src="/public/image/RM88.jpg" alt="percuma rm88"></a>
 			</article>
 
@@ -89,16 +89,16 @@
 		<div class="d-block d-md-none">
 			<article>
 				<!--baccarat largest bonus-->
-				<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img width="100%"
+				<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="100%"
 						src="/public/image/challenge.jpg" alt="baccarat largest bonus"></a>
 
 				<!--Share & Downline & Copy Link button-->
 				<div class="btn_left">
-					<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img width="50%"
+					<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="50%"
 							src="/public/image/share_btn.gif" alt="Share"></a>
-					<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img width="50%"
+					<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="50%"
 							src="/public/image/downline_btn.gif" alt="Downline"></a>
-					<a href="https://www.atascasinobet1.com/sign-up" rel="nofollow" target="_blank"><img width="50%"
+					<a :href="link + '/sign-up'" rel="nofollow" target="_blank"><img width="50%"
 							src="/public/image/copylink_btn.gif" alt="Copy Link button"></a>
 				</div>
 
@@ -123,8 +123,33 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
 	name: 'Right',
+	data() {
+		return {
+			link: ""
+		}
+	},
+	methods: {
+		async calllink() {
+			try {
+				const response = await axios.get('https://seo.mobileapplab.online/api/atas?fields[0]=atascasinobet_com', {
+					headers: {
+						"Authorization": "Bearer " + "1c4db3188ab2e9a077928920d9cc8d3322d15f9751bc2054a5cb70008df79cf3e3a4dd005a75a1f2db40eb953292ee10ef699693e96e9d77a98439f438ee6a6e6805a8a955e992f082b9e6118a4345e1ed18438ff9789edf9ed1dd58af45ee6669a7519a1291746959ff45bc2054b7f408b5da5ea8cd04d588a2704b7e218021",
+					}
+				});
+				this.link = response.data.data.attributes.atascasinobet_com;
+
+				console.log(this.link);
+			} catch (error) {
+				console.error(error);
+			}
+		},
+	},
+	mounted() {
+		this.calllink();
+	}
 };
 </script>
 
