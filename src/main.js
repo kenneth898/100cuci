d425f9982d3600bcfa87d9120ce4bc33460c17f4
kenneth898 <kennethtan0601@gmail.com>
2024-09-11 19:20:22 +0800

    api sitemap

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; // Import your router instance
-import i18n from './i18n'; // Import your i18n instance
+import router from './router';
+import i18n from './i18n';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-// Create and mount the app with router and i18n
 createApp(App)
 	.use(router)
 	.use(i18n)
+	.use(store)
 	.mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -345,6 +345,7 @@
 
 <script>
 import axios from 'axios';
+import { mapGetters } from 'vuex';
 import Left from '../components/Left.vue';
 import Right from '../components/Rightside.vue';
 export default {
@@ -355,29 +356,17 @@ export default {
 	},
 	data() {
 		return {
-			link: "",
 			link2: ""
 		}
+	},
+	computed: {
+		...mapGetters(['link'])
 	},
 	methods: {
 		scrollTo(id) {
 			const element = document.getElementById(id);
 			if (element) {
 				element.scrollIntoView({ behavior: 'smooth' });
-			}
-		},
-		async calllink() {
-			try {
-				const response = await axios.get('https://seo.mobileapplab.online/api/atas?fields[0]=atascasinobet_com', {
-					headers: {
-						"Authorization": "Bearer " + "1c4db3188ab2e9a077928920d9cc8d3322d15f9751bc2054a5cb70008df79cf3e3a4dd005a75a1f2db40eb953292ee10ef699693e96e9d77a98439f438ee6a6e6805a8a955e992f082b9e6118a4345e1ed18438ff9789edf9ed1dd58af45ee6669a7519a1291746959ff45bc2054b7f408b5da5ea8cd04d588a2704b7e218021",
-					}
-				});
-				this.link = response.data.data.attributes.atascasinobet_com;
-
-				console.log(this.link);
-			} catch (error) {
-				console.error(error);
 			}
 		},
 		async calllink2() {
@@ -398,7 +387,7 @@ export default {
 
 	},
 	mounted() {
-		this.calllink();
+		this.$store.dispatch('fetchLink');
 		this.calllink2();
 	}
 
